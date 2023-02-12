ce498009742da3db58f93709a6ffb20fb80e6801
the-rooster <andrewmjculberson@gmail.com>
2023-02-11 18:34:49 -0700

    making templates work

--- InstaTonne/urls.py
+++ InstaTonne/urls.py
@@ -38,6 +38,7 @@ router.register(r'users', UserViewSet)
 urlpatterns = [
     path('service/',include("InstaTonneApis.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('openapi', get_schema_view(
