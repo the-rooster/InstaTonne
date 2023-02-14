d4db6d5e6ba201ae810312ab04096700f7a63c94
the-rooster <andrewmjculberson@gmail.com>
2023-02-13 18:48:37 -0700

    password fields are hidden

--- InstaTonne/forms.py
+++ InstaTonne/forms.py
@@ -4,10 +4,10 @@ from django import forms
 class RegisterForm(forms.Form):
 
     username = forms.CharField(label="Username",max_length=20)
-    password = forms.CharField(label="Password",min_length=8)
-    check_password = forms.CharField(label="Password Again")
+    password = forms.CharField(label="Password",min_length=8,widget=forms.PasswordInput())
+    check_password = forms.CharField(label="Password Again",widget=forms.PasswordInput())
 
 class LoginForm(forms.Form):
 
     username = forms.CharField(label="Username",max_length=20)
-    password = forms.CharField(label="Password",min_length=8)
+    password = forms.CharField(label="Password",min_length=8,widget=forms.PasswordInput())
