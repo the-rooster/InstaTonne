04f0badd4f47fb7caf75cebcf676a56dd67c67e6
Logan Vaughan <logan.v90@gmail.com>
2023-02-20 17:57:41 -0700

    refactor posts and likes and comments

--- InstaTonneApis/endpoints/utils.py
+++ InstaTonneApis/endpoints/utils.py
@@ -16,3 +16,15 @@ def valid_requesting_user(request: HttpRequest, required_author_id: int) -> bool
         return False
 
     return True
+
+
+def make_post_url(request_host: str, author_id: int, post_id: int) -> str:
+    return "http://" + request_host + "/service/authors/" + str(author_id) + "/posts/" + str(post_id)
+
+
+def make_comments_url(request_host: str, author_id: int, post_id: int) -> str:
+    return make_post_url(request_host, author_id, post_id) + "/comments"
+
+
+def make_comment_url(request_host: str, author_id: int, post_id: int, comment_id: int) -> str:
+    return make_post_url(request_host, author_id, post_id) + "/comments/" + str(comment_id)
