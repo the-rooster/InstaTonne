8a9ee3039106a292a35f1fca25a3a303785dfd66
the-rooster <andrewmjculberson@gmail.com>
2023-02-20 17:02:43 -0700

    likes working for inbox now

--- InstaTonne/settings.py
+++ InstaTonne/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Configure this before deployment
+HOSTNAME = "localhost:8000"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
