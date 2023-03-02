b0ec059d6d6276bcbf5fa2434b7681c70bb31be9
the-rooster <andrewmjculberson@gmail.com>
2023-03-02 09:24:54 -0700

    initial spec generated by postman-to-openapi

---
+++ InstaTonne-FrontEnd/vue.config.ts
@@ -0,0 +1,12 @@
+publicPath: 'http://localhost:8080' //base url where app gets deployed!
+outputDir: '../static/dist' //path for static files when built
+indexPath: '../templates/_base_vue.html' //path for generated index file that django will use
+
+
+configureWebPack: {
+    devServer: {
+        devMiddleware: {
+            writeToDisk: true
+        }
+    }
+}
