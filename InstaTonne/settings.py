0b678d57f2ec63184f9fa30de3f0c6f918808e7b
the-rooster <andrewmjculberson@gmail.com>
2023-03-19 20:54:14 -0600

    stuff fixed

--- InstaTonne/settings.py
+++ InstaTonne/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"] if "SECRET_KEY" in os.environ else 'django
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['instatonne-cmput404.herokuapp.com']
+ALLOWED_HOSTS = ['instatonne-cmput404.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -155,8 +155,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+print("BASE DIR",BASE_DIR)
 STATIC_URL = 'assets/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/assets/')
+STATIC_ROOT = BASE_DIR / 'assets_go_here'
+print("STATIC ROOT",STATIC_ROOT)
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'templates/assets',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
