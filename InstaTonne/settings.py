10f52a484c7bf2fbbed9bc5e2b7e24157d1b69d7
the-rooster <andrewmjculberson@gmail.com>
2023-03-21 11:50:33 -0600

    csrf exempted posting to inbox. changed default dev variables to be nicer

--- InstaTonne/settings.py
+++ InstaTonne/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 #Configure this before deployment
 HOSTNAME = os.environ["HOSTNAME"] if "HOSTNAME" in os.environ else "http://127.0.0.1:8000"
-FRONTEND = os.environ["FRONTEND"] if "FRONTEND" in os.environ else "http://127.0.0.1:8000"
+FRONTEND = os.environ["FRONTEND"] if "FRONTEND" in os.environ else "http://127.0.0.1:5173"
 
 print(HOSTNAME,FRONTEND)
 # Quick-start development settings - unsuitable for production
