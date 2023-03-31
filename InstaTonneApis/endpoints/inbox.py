49097ff97656155feace17ade55a1f16bb7b945a
Logan Vaughan <logan@Logans-MacBook-Pro.local>
2023-03-31 12:47:08 -0600

    adding example values to swagger

--- InstaTonneApis/endpoints/inbox.py
+++ InstaTonneApis/endpoints/inbox.py
@@ -160,7 +160,7 @@ def get_ginbox(request: HttpRequest, author_id: str):
     all_data.sort(key=lambda x: x['created_at'], reverse=True)
 
     res = json.dumps({
-        "type" : "inbox",
+        "type" : "ginbox",
         "author" : author.id_url,
         "items" : all_data
     })
