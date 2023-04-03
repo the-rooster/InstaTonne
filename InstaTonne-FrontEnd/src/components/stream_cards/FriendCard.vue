7e71b8bfb817ede63186e451c2ae51d9023cabd8
Martin Rudolf <martinwrudolf@gmail.com>
2023-04-03 10:48:20 -0600

    Updated some UI stuff
    Updated the Friends/Followers page to look cleaner and added modals. Also changed AuthorCard displayName

--- InstaTonne-FrontEnd/src/components/stream_cards/FriendCard.vue
+++ InstaTonne-FrontEnd/src/components/stream_cards/FriendCard.vue
@@ -1,15 +1,12 @@
 <template>
   <v-card
-    class="mx-auto my-5 rounded-l"
+    class="mx-auto my-5 rounded-xl"
     color="#E3F2FD"
     theme="light"
     max-width="80%"
   >
     <v-card-actions>
-      <v-list-item
-        v-if="loading"
-        class="w-100"
-      >
+      <v-list-item v-if="loading" class="w-100">
         <v-progress-circular
           indeterminate
           width="20"
@@ -17,10 +14,7 @@
           class="loadingIcon"
         />
       </v-list-item>
-      <v-row
-        v-else
-        class="flex-container"
-      >
+      <v-row v-else class="flex-container">
         <v-col>
           <router-link
             id="router"
@@ -28,10 +22,7 @@
             class="flex-container"
           >
             <v-col>
-              <img
-                class="profile-picture"
-                :src="requestData.profileImage"
-              >
+              <img class="profile-picture" :src="requestData.profileImage" />
             </v-col>
             <v-col class="d-flex justify-center align-center">
               <div>
@@ -41,11 +32,20 @@
             </v-col>
           </router-link>
         </v-col>
-        <v-col
-          cols="3"
-          class="d-flex justify-center align-center"
-        >
-          <v-btn @click="removeFriend">
+        <v-col cols="3" class="d-flex justify-center align-center">
+          <ConfirmationModal
+            ref="showConfirmation"
+            message="Are you sure you want to remove this friend?"
+            @selected="(value) => removeFriend(value)"
+          />
+          <v-btn
+            class="remove-button"
+            @click="
+              () => {
+                showConfirmation.show = true;
+              }
+            "
+          >
             Remove
           </v-btn>
           {{ error }}
@@ -58,6 +58,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { createHTTP } from "../../axiosCalls";
+import ConfirmationModal from "../ConfirmationModal.vue";
 
 const loading = ref(false);
 
@@ -73,18 +74,23 @@ const props = defineProps({
 });
 
 const emit = defineEmits(["update"]);
+const showConfirmation = ref(false);
 
 const error = ref("");
 
-async function removeFriend() {
-  loading.value = true;
-  const foreignId = encodeURIComponent(props.requestData.id);
-  await createHTTP(`authors/${props.authorId}/followers/${foreignId}`)
-    .delete(JSON.stringify({}))
-    .then(() => {
-      emit("update");
-      loading.value = false;
-    });
+async function removeFriend(value: boolean) {
+  if (value) {
+    loading.value = true;
+    const foreignId = encodeURIComponent(props.requestData.id);
+    await createHTTP(`authors/${props.authorId}/followers/${foreignId}`)
+      .delete(JSON.stringify({}))
+      .then(() => {
+        emit("update");
+        loading.value = false;
+      });
+  }
+  showConfirmation.value = false;
+  window.location.reload();
 }
 </script>
 
@@ -98,6 +104,17 @@ async function removeFriend() {
   width: 100%;
   justify-content: space-around;
 }
+.profile-picture {
+  width: 16vw;
+  height: 16vw;
+  border-radius: 100%;
+}
+
+.remove-button {
+  background-color: #ff9797;
+  color: #000000;
+}
+
 #router {
   width: px;
 }
